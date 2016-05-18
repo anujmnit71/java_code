d4b7672df8a76b281ce584b3e778323114c690d2
U-AMERICAS\Anuj_K_Verma <Anuj_K_Verma@WN7X64-CJBFT12.blr.amer.dell.com>
2016-05-18 22:32:16 +0530

    Added collection framework

--- Collection_framework/src/aldemo.java
+++ Collection_framework/src/aldemo.java
@@ -1,30 +1,31 @@
-/*import java.util.ArrayList;
-
+import java.util.Vector;
 
 
 public class aldemo {
 
-	public static void test(int[] a ){
+	public static void test(Integer[] a ){
 		System.out.print(a);
 	}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Integer> al = new ArrayList<Integer>();
+		Vector<Vector<Integer>> all = new Vector<Vector<Integer>>();
+		Vector<Integer>  current= new Vector<Integer>();
 		for(int i=0 ; i<10 ; i++)
-			al.add(i);
-		System.out.println(al);
-		
-		Integer[] a = new Integer[al.size()];
+			current.add(i);
+		//System.out.println(aldemo);
+		all.add(current);
+		all.add(current);
+		System.out.print("hellob");
+		/*Integer[] a = new Integer[al.size()];
 		al.toArray(a);
 		aldemo.test(a);
 		
 		for(int i=0 ; i<10 ; i++)
 		System.out.print(a[i]);
-		
+		*/
 		
 	}
 
 }
-*/
